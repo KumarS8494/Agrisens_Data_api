232f1adfc0c5930052d49b58004623efb0d853c3
Yuvraj171 <yuvrajkulwant2002@gmail.com>
2024-12-13 10:24:05 +0530

    test port number

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from routes import spectral_routes  
+import os
 
 app = Flask(__name__)
 
@@ -10,4 +11,5 @@ def home():
     return {"message": "Welcome to the Spectral Values API!"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.getenv("PORT", 5000))  # Default to port 5000 if not provided
+    app.run(host="0.0.0.0", port=port)
